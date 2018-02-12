1f752aac8590dfc5f2e90e2a694e7a8fe0a1aaa5
Clarence Wang <charlesfoxw@gmail.com>
2018-02-11 23:02:14 -0800

    Randomize Ground

--- Peter Pan's Dream/Assets/Scripts/PlatformGenerator.cs
+++ Peter Pan's Dream/Assets/Scripts/PlatformGenerator.cs
@@ -10,6 +10,9 @@ public class PlatformGenerator : MonoBehaviour {
 
 	private float platformWidth;
 
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+
 	// Use this for initialization
 	void Start () {
 		platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x; 
@@ -18,6 +21,8 @@ public class PlatformGenerator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (transform.position.x < generationPoint.position.x){
+			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+
 			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween,transform.position.y, transform.position.z);
 			Instantiate (thePlatform, transform.position, transform.rotation);
 		}
