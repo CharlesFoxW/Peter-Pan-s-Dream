c1410b6ee02c696199dc048b33989a0ba34dbe63
Felicity-GYW <felicity@Felicity-GYWs-MacBook-Pro.local>
2018-02-14 10:30:18 -0800

    bg

---
+++ Peter Pan's Dream/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public CameraController cc;
+	public GameObject background2;
+
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cc.transform.position.x >= this.transform.position.x) {
+			background2.transform.position = new Vector3 (
+				this.transform.position.x + this.transform.localScale.x,
+				background2.transform.position.y,
+				background2.transform.position.z
+			);
+		}
+		
+	}
+}
