239a40759382765036e3a6038afd50ee744c190e
Felicity_GYW <gyw0gyw@gmail.com>
2018-03-07 09:01:31 -0800

    make daytime a little bit easier to play

--- Peter Pan's Dream/Assets/Scripts/DaytimeControl.cs
+++ Peter Pan's Dream/Assets/Scripts/DaytimeControl.cs
@@ -6,7 +6,7 @@ public class DaytimeControl : MonoBehaviour {
 
     public GameObject myFade;
     public static float timepast = 0f;
-    private float timebar = 60f;
+    private float timebar = 30f;
 
     private ScoreManager myScoreManager;
 
